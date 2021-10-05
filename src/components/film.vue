009e4b9eacc74426dcd2fcfa83f52cd0aa616cea
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-05 23:25:13 +0200

    aggiunta bandiera al posto della lingua

--- src/components/film.vue
+++ src/components/film.vue
@@ -2,7 +2,9 @@
 <div class="film">
     <div class="titolo">{{info.title}}</div>
     <div class="titolo-originale">{{info.original_title}}</div>
-    <div class="lingua">{{info.original_language}}</div>
+    <div class="lingua">
+        <img :src="`https://www.unknown.nu/flags/images/${info.original_language}-100`" alt="">
+    </div>
     <div class="voto">{{info.vote_average}}</div>
 </div>
 </template>
